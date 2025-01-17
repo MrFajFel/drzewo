c12b1b30a3d01301ad9b304bde45914b76460f0b
Kinga Smolarek <100038018+Radio2840@users.noreply.github.com>
2025-01-17 11:25:11 +0100

    drzewo na nastepna lekcje

--- Tree.py
+++ Tree.py
@@ -1,10 +1,28 @@
+from enum import nonmember
+
 from TreeNode import TreeNode
 
 
 class Tree:
-    def __init__(self,rootValue):
-        self.root = TreeNode(rootValue,None,None,None)
+    def __init__(self,root_value):
+        self.root = TreeNode(root_value,None,None,None)
 
 
-    def read_from_file():
+    def read_from_file(self,filename):
         pass
+
+    def add_node(self,node,parent_node):
+        if parent_node.left is None:
+            parent_node.left = node
+        elif parent_node.right is None:
+            parent_node.right = node
+        else:
+            raise Exception("Node is full")
+
+    def dfs_show(self, node):
+        print(node.value)
+        if node.left is not None:
+            self.dfs_show(node.left)
+        if node.right is not None:
+            self.dfs_show(node.right)
+        return
---
+++ main.py
@@ -0,0 +1,20 @@
+from Tree import Tree
+from TreeNode import TreeNode
+
+tree = Tree(7)
+node1 = TreeNode(5,None,None,tree.root)
+tree.add_node(node1,tree.root)
+
+node2 = TreeNode(2,None,None,tree.root)
+tree.add_node(node2,tree.root)
+
+node3 = TreeNode(7,None,None,node1)
+tree.add_node(node3,node1)
+
+node4 = TreeNode(6,None,None,node1)
+tree.add_node(node4,node1)
+
+node5 = TreeNode(3,None,None,node2)
+tree.add_node(node5,node2)
+
+tree.dfs_show(tree.root)
